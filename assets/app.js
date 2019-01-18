e6a95b1dad30d16b973280e19bcad49712152a65
prestonedwardsjr <pres@imdiversity.com>
2019-01-17 20:38:37 -0600

    inital commit

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+alert( "You're now connected");
+
+ src="https://www.gstatic.com/firebasejs/5.7.3/firebase.js"
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBSh-Q96IW45BqnABhWBh6e4kbT_M722Sg",
+    authDomain: "my-trainscheduler.firebaseapp.com",
+    databaseURL: "https://my-trainscheduler.firebaseio.com",
+    projectId: "my-trainscheduler",
+    storageBucket: "my-trainscheduler.appspot.com",
+    messagingSenderId: "581549746591"
+  };
+  firebase.initializeApp(config);
